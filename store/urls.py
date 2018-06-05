9a697c8b6d6c22c71c47ccdea20569fb688e3ed6
Yusuf Musleh <yusuf.musleh@gmail.com>
2018-06-06 00:16:43 +0300

    Move slash to end of api-v1 in urls instead of before all the routes for DRF

--- store/urls.py
+++ store/urls.py
@@ -7,19 +7,19 @@ from store.viewsets import *
 
 router = routers.DefaultRouter()
 
-router.register(r'/products', products.ProductViewSet, base_name='products')
-router.register(r'/categories', products.CategoryViewSet, base_name='categories')
-router.register(r'/subcategories', products.SubcategoryViewSet, base_name='subcategories')
+router.register(r'products', products.ProductViewSet, base_name='products')
+router.register(r'categories', products.CategoryViewSet, base_name='categories')
+router.register(r'subcategories', products.SubcategoryViewSet, base_name='subcategories')
 
-router.register(r'/stores', stores.StoreViewSet, base_name='stores')
+router.register(r'stores', stores.StoreViewSet, base_name='stores')
 
-router.register(r'/customers', customers.CustomerViewSet, base_name='customers')
+router.register(r'customers', customers.CustomerViewSet, base_name='customers')
 
-router.register(r'/orders', orders.OrderViewSet, base_name='orders')
+router.register(r'orders', orders.OrderViewSet, base_name='orders')
 
 urlpatterns = [
     # REST framework
-    path('api-v1', include(router.urls)),
+    path('api-v1/', include(router.urls)),
     # Should be last because matches everything
     re_path(r'', IndexPageView.as_view(), name='index')
 ]
