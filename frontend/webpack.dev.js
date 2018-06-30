017200b9cf40db03ab632dc5d5bdfa96b14bbd74
Yusuf Musleh <yusuf.musleh@gmail.com>
2018-06-30 14:39:25 +0300

    Add file-loader and image-webpack-loader to webpack Handle loading and compression of files/imgs in dev/prod

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -9,4 +9,43 @@ module.exports = WebpackMerge(common, {
     plugins: [
         new BundleTracker({filename: "../webpack-dev-stats.json"}),
     ],
-})
+
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|svg|woff|woff2|eot|ttf|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name]-[hash].[ext]',
+                            hash: 'sha512',
+                            digest: 'hex'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            optipng: {
+                                optimizationLevel: 3,
+                            },
+                            pngquant: {
+                                quality: '90-100',
+                                speed: 1
+                            },
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            gifsicle: {
+                                interlaced: true,
+                                optimizationLevel: 3
+                            },
+                        }
+                    }
+                ]
+            },
+        ]
+    }
+
+});
--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -18,4 +18,42 @@ module.exports = WebpackMerge(common, {
             minRatio: 0.8
         }),
     ],
-})
+
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|svg|woff|woff2|eot|ttf|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name]-[hash].[ext]',
+                            hash: 'sha512',
+                            digest: 'hex'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            optipng: {
+                                optimizationLevel: 7,
+                            },
+                            pngquant: {
+                                quality: '80-85',
+                                speed: 1
+                            },
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            gifsicle: {
+                                interlaced: true,
+                                optimizationLevel: 3
+                            },
+                        }
+                    }
+                ]
+            },
+        ]
+    }
+});
