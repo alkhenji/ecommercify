b2f6e556502d9eb53bbd4f87b827eb3b37c5ff4b
Yusuf Musleh <yusuf.musleh@gmail.com>
2018-09-10 01:57:18 +0300

    Add 'Add to Cart' functionality to ProductPage Add LoadingSpinner to ProductPage when loading page

--- frontend/js/pages/ProductPage.js
+++ frontend/js/pages/ProductPage.js
@@ -2,18 +2,42 @@
 
 import React from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux'
 
-import type { ProductType } from '../flowtypes';
+import { addProductToCart, updateProductDataInCart } from '../redux/actions/Cart';
 
-type PropsType = {};
+import LoadingSpinner from '../components/LoadingSpinner';
+
+import type { ProductType, CartProductType } from '../flowtypes';
+
+type PropsType = {
+  cartProducts: Array<CartProductType>,
+  addingProduct: boolean,
+  error: Object,
+  addProductToCart: (product: ProductType, quantity: number) => {},
+  updateProductDataInCart: (product: CartProductType, data: Object) => {}
+};
 type StateType = {
   product: ProductType | Object | null,
+  loading: boolean
 };
 
-export default class ProductPage extends React.Component<ProductType, StateType> {
+const mapStateToProps = state => ({
+  cartProducts: state.cartProducts,
+  addingProduct: state.addingProduct,
+  error: state.error,
+});
+
+const mapDispatchToProps = dispatch => ({
+  addProductToCart: (product, quantity) => dispatch(addProductToCart(product, quantity)),
+  updateProductDataInCart: (product, data) => dispatch(updateProductDataInCart(product, data)),
+});
+
+class ProductPage extends React.Component<ProductType, StateType> {
 
   state: StateType = {
     product: {},
+    loading: true,
   }
 
   componentDidMount() {
@@ -24,12 +48,14 @@ export default class ProductPage extends React.Component<ProductType, StateType>
   fetchProductDetails(productSlug: string) {
     axios.get('/api-v1/products/'+ productSlug + '/').then(response => {
       this.setState({
-        product: response.data
+        product: response.data,
+        loading: false
       });
     }).catch(error => {
       console.error(error);
       this.setState({
-        product: null
+        product: null,
+        loading: false
       });
     });
   }
@@ -38,8 +64,24 @@ export default class ProductPage extends React.Component<ProductType, StateType>
     return thumbnail ? thumbnail.image : 'http://via.placeholder.com/500x500';
   }
 
+  handleAddProductToCart(product: ProductType, quantity: number) {
+    const { cartProducts, addProductToCart, updateProductDataInCart } = this.props;
+
+    /* we check if product exists in cart */
+    for (var i = 0; i < cartProducts.length; i++) {
+      if (product.slug === cartProducts[i].product.slug) {
+        /* product exists in cart, update it's quantity */
+        updateProductDataInCart(cartProducts[i], {quantity: cartProducts[i].quantity + quantity})
+        return;
+      }
+    }
+    /* new product not found in cart, add to cart */
+    addProductToCart(product, quantity);
+  }
+
   render() {
-    const { product } = this.state;
+    const { product, loading } = this.state;
+    const { addingProduct } = this.props;
 
     if (product == null) {
       return <h1>No such product.</h1>
@@ -47,6 +89,7 @@ export default class ProductPage extends React.Component<ProductType, StateType>
 
     return (
       <div style={styles.container} className='container'>
+        { loading ? <LoadingSpinner /> : null }
         <div className='row featurette'>
           <div className='col-md-5 order-md-1'>
             <img className='featurette-image img-fluid mx-auto' src={ this.determineImageUrl(product.thumbnail) } data-holder-rendered='true' />
@@ -60,7 +103,8 @@ export default class ProductPage extends React.Component<ProductType, StateType>
             <button
               type='button'
               className='btn btn-lg btn-outline-secondary'
-              disabled={ product.quantity == 0 }>
+              onClick={ () => this.handleAddProductToCart(product, 1) }
+              disabled={ product.quantity == 0 || addingProduct }>
                 { product.quantity != 0 ? <span>Add to Cart</span> : <span>Out of Stock</span> }
             </button>
           </div>
@@ -73,6 +117,10 @@ export default class ProductPage extends React.Component<ProductType, StateType>
 const styles: Object = {
   container: {
     paddingTop: 30,
-    paddingBottom: 30
+    paddingBottom: 30,
+    position: 'relative',
+    minHeight: 300,
   }
 };
+
+export default connect(mapStateToProps, mapDispatchToProps) (ProductPage);
